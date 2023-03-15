d4477512099aec8903ed95c60a1522b19149379a
osohyun0224 <osohyun0224@naver.com>
2023-03-15 12:35:36 +0900

    0315

---
+++ 0315/중앙값구하기.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] array) {
+        Arrays.sort(array);
+        int answer = array[array.length/2];
+        return answer;
+    }
+}
