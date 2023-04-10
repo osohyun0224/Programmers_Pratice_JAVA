3027c9a77cef480a3351534f02f7a8852c0b69df
오소현 <53892427+osohyun0224@users.noreply.github.com>
2023-04-10 15:38:07 +0900

    [unrated] Title: 공원 산책, Time: 0.06 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/unrated/172928. 공원 산책/공원 산책.java
+++ 프로그래머스/unrated/172928. 공원 산책/공원 산책.java
@@ -8,77 +8,74 @@ class Solution {
 				if(park[i].charAt(j) == 'S') {
 					answer[0] = i;
 					answer[1] = j;
-					System.out.println(answer[0] + " / " + answer[1]);
 				}
 			}
 		}
-		
-		
 		for(int i=0; i<routes.length; i++) {
 			
 			if(routes[i].charAt(0) == 'E') {
-				int move = Integer.valueOf(routes[i].charAt(2)-48);
+				int now = Integer.valueOf(routes[i].charAt(2)-48);
 				
 				boolean flag = false;
 				
-				if(answer[1] + move >= park[answer[0]].length()) continue;
-				for(int j=1; j<=move; j++) {
+				if(answer[1] + now >= park[answer[0]].length()) continue;
+				for(int j=1; j<=now; j++) {
 					if(park[answer[0]].charAt(answer[1]+j) == 'X') {
 						flag = true;
 						break;
 					}
 				}
 				if(flag == true) continue;
-				answer[1] += move;
+				answer[1] += now;
 			}
 			
 			else if(routes[i].charAt(0) == 'W') { 
-				int move = Integer.valueOf(routes[i].charAt(2)-48);
+				int now = Integer.valueOf(routes[i].charAt(2)-48);
 				
 				boolean flag = false;
 				
-				if(answer[1] - move < 0) continue; 
-				for(int j=1; j<=move; j++) { 
+				if(answer[1] - now< 0) continue; 
+				for(int j=1; j<=now; j++) { 
 					if(park[answer[0]].charAt(answer[1]-j) == 'X') {
 						flag = true;
 						break;
 					}
 				}
 				if(flag == true) continue;
-				answer[1] -= move;
+				answer[1] -= now;
 
 			}
 			
 			else if(routes[i].charAt(0) == 'S') { 
-				int move = Integer.valueOf(routes[i].charAt(2)-48);
+				int now = Integer.valueOf(routes[i].charAt(2)-48);
 				
 				boolean flag = false;
 				
-				if(answer[0] + move >= park.length) continue; 
-				for(int j=1; j<=move; j++) {
+				if(answer[0] + now >= park.length) continue; 
+				for(int j=1; j<=now; j++) {
 					if(park[answer[0]+j].charAt(answer[1]) == 'X') {
 						flag = true;
 						break;
 					}
 				}
 				if(flag == true) continue;
-				answer[0] += move;
+				answer[0] += now;
 			}
 			
 			else if(routes[i].charAt(0) == 'N') { 
-				int move = Integer.valueOf(routes[i].charAt(2)-48);
+				int now = Integer.valueOf(routes[i].charAt(2)-48);
 				
 				boolean flag = false;
 				
-				if(answer[0] - move < 0) continue; 
-				for(int j=1; j<=move; j++) {
+				if(answer[0] - now < 0) continue; 
+				for(int j=1; j<=now; j++) {
 					if(park[answer[0]-j].charAt(answer[1]) == 'X') {
 						flag = true;
 						break;
 					}
 				}
 				if(flag == true) continue;
-				answer[0] -= move;
+				answer[0] -= now;
 			}
 		}
 		
