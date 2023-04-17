31a8256abe5b065295e2e2fee3c0f79997fdee9e
오소현 <53892427+osohyun0224@users.noreply.github.com>
2023-04-17 16:11:49 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 70.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -2,9 +2,8 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         while(n>0){
-            answer += n%10;
-            
-            n/=10;
+            answer+=n%10;
+            n /= 10;
         }
         return answer;
     }
