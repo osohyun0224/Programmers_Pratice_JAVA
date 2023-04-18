055c05ae688f8ed4ed926550bc45a6b068b5b720
오소현 <53892427+osohyun0224@users.noreply.github.com>
2023-04-18 22:07:52 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.03 ms, Memory: 85.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int chicken) {
+        int coupon = chicken;
+		int count = 0;
+
+		while (coupon >= 10) {
+			int leftCoupon = coupon % 10;
+			count += coupon / 10;
+			coupon = leftCoupon + coupon / 10;
+		}
+		return count;
+    }}
